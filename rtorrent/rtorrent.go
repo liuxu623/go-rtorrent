bc57dd22f7019deb5aeb080516d6014a583510a2
liuxu <liuxu623@gmail.com>
2019-12-29 14:23:32 +0800

    add finished

--- rtorrent/rtorrent.go
+++ rtorrent/rtorrent.go
@@ -36,6 +36,7 @@ type Status struct {
 	UpRate         int
 	Ratio          float64
 	Size           int
+	Finished       time.Time
 }
 
 // File represents a file in rTorrent
@@ -361,5 +362,11 @@ func (r *RTorrent) GetStatus(t Torrent) (Status, error) {
 		return s, errors.Wrap(err, "d.size_bytes XMLRPC call failed")
 	}
 	s.Size = results.([]interface{})[0].(int)
+	// Finished
+	results, err = r.xmlrpcClient.Call("d.timestamp.finished", t.Hash)
+	if err != nil {
+		return s, errors.Wrap(err, "d.timestamp.finished XMLRPC call failed")
+	}
+	s.Finished = time.Unix(int64(results.([]interface{})[0].(int)), 0)
 	return s, nil
 }
